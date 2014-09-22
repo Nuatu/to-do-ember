19a014d326acbabd2fcbe19e08372a02fd9d4227
Nuatu Tseggai & Sterling Houghton <student@epicodus.com>
2014-09-22 14:02:54 -0700

    add items remaining functionality

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,8 +1,6 @@
 Todos.TodosController = Ember.ArrayController.extend({
   actions: {
 
-
-
     createTodo: function() {
       // Get the todo title set by the "New Todo" text field
       var title = this.get('newTitle');
@@ -21,7 +19,15 @@ Todos.TodosController = Ember.ArrayController.extend({
       // Save the new model
       todo.save();
     }
-  }
+  },
 
+  remaining: function() {
+    return this.filterBy('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  inflection: function() {
+    var remaining = this.get('remaining');
+    return remaining === 1? 'item' : 'items';
+  }.property('remaining')
 
 });
