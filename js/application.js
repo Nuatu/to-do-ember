dd73c8cde701f7c4c397e93d593cf84996949536
Nuatu Tseggai & Sterling Houghton <student@epicodus.com>
2014-09-23 12:04:10 -0700

    add EmberFire backend configurations

--- js/application.js
+++ js/application.js
@@ -1,5 +1,11 @@
 window.Todos = Ember.Application.create();
 
-Todos.ApplicationAdapter = DS.LSAdapter.extend({
-  namespace: 'todos-emberjs'
+
+Todos.ApplicationAdapter = DS.FirebaseAdapter.extend({
+  firebase: new Firebase('https://to-do-ember.firebaseio.com/')
 });
+
+
+// Todos.ApplicationAdapter = DS.LSAdapter.extend({
+//   namespace: 'todos-emberjs'
+// });
